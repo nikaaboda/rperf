0a8231fe75449a3b2595a5c8f60b8ec2a4c72588
Nikolozi Bodaveli <n.bodaveli@jacobs-university.de>
2023-05-15 17:43:39 +0200

    another minor fix

--- src/protocol/messaging.rs
+++ src/protocol/messaging.rs
@@ -529,7 +529,7 @@ pub fn prepare_download_configuration(
             log::warn!("requested receive-buffer, {}, is too small to hold the data to be exchanged; it will be increased to {}", receive_buffer, length * 2);
             receive_buffer = length * 2;
         }
-        Ok(prepare_configuration_tls_download(
+        Ok(prepare_configuration_ktls_download(
             test_id,
             parallel_streams,
             length as usize,
